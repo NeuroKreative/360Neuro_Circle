30c6016d45c085bd0427eb502f9eaeb28e78b809
Fernando V Santos <51957805+NeuroKreative@users.noreply.github.com>
2025-09-19 11:46:30 -0700

    Update circle_api.py

--- app/circle_api.py
+++ app/circle_api.py
@@ -5,7 +5,7 @@ CIRCLE_API_URL = "https://app.circle.so/api/v1"
 def fetch_token(email, password):
     response = requests.post(f"{CIRCLE_API_URL}/auth/login", json={"email": email, "password": password})
     return response.json().get("token")
-
+'''
 def get_auth_token():
     email = os.getenv("CIRCLE_EMAIL")
     password = os.getenv("CIRCLE_PASSWORD")
@@ -16,3 +16,4 @@ def fetch_posts(token):
     headers = {"Authorization": f"Bearer {token}"}
     response = requests.get(f"{CIRCLE_API_URL}/posts", headers=headers)
     return response.json()
+'''
